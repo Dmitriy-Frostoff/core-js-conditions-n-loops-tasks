c6efd656b16f1077fc331c832ccedc2549b45a6b
dmitriy-frostoff <dmitriy.frostoff@yandex.ru>
2024-02-09 23:43:34 +0300

    feat: implement convertToRomanNumerals function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -129,8 +129,32 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  const dictionaryOfArabicRomanNums = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+  };
+
+  if (num <= 10) {
+    return dictionaryOfArabicRomanNums[num];
+  }
+
+  const unitsArabic = num % 10;
+  const tensArabic = num - (num % 10);
+  const unitsRoman = dictionaryOfArabicRomanNums[unitsArabic] ?? '';
+  const tensRoman = dictionaryOfArabicRomanNums[tensArabic] ?? '';
+
+  return `${tensRoman}${unitsRoman}`;
 }
 
 /**
